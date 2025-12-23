8442b9710b57e23e9a05a03edebfc66ae093a810
Anuj Kumar <xp27anujkumar@gmail.com>
2025-12-23 18:28:54 +0530

    update

--- src/utils/base_url.jsx
+++ src/utils/base_url.jsx
@@ -1,3 +1,3 @@
 export const base_url = 'https://api.ngoguru.in/api/v1';
-export const base_client_url='http://localhost:5900/api/v1';
-// export const base_client_url = 'https://ssspmsascnandur-backend.onrender.com/api/v1';
+//export const base_client_url='http://localhost:5900/api/v1';
+ export const base_client_url = 'https://ssspmsascnandur-backend.onrender.com/api/v1';
